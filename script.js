9a1a98e373f6d0d9a48e1deda46f533a087b83ac
Epshtein Dmitry <letuver@yahoo.com>
2023-07-28 03:32:32 +0300

    string function

---
+++ script.js
@@ -0,0 +1,21 @@
+"use strict"
+
+const sliteFunc = function (string, length) {
+    return string.substring(0, length) + "...";
+}
+
+const strFunc = function (arg) {
+    typeof arg != "string"
+        ? (
+            console.log("Not string")
+        ) : (
+            console.log("Argument is string"),
+            arg = arg.trim(),
+            arg = arg.length > 30
+                ? sliteFunc(arg, 30)
+                : arg
+            )
+    return arg;
+};
+
+console.log(strFunc("                fffqwdkdkkdQEQWQWdhsfhaskdhsafjlksfKFJ           "));
